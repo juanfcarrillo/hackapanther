7d509146eecac5a736b75616f3f7e9703c59ca76
juanfcarrillo <juanfi444@outlook.com>
2024-01-21 20:07:39 -0500

    feat(code section): add example case

--- src/app/components/CodeSection/CodeSection.tsx
+++ src/app/components/CodeSection/CodeSection.tsx
@@ -4,13 +4,19 @@ import { Editor } from "@monaco-editor/react";
 import oneDarkProTheme from "../../../assets/oneDarkPro.json";
 import OutputCode from "./OutputCode";
 
+const defaultValue = `
+function twoSum(nums, target) {
+    return []
+}
+`
+
 export default function CodeSection(): JSX.Element {
     return (
         <section className="flex-1 relative overflow-hidden">
             <Editor
                 height="100%"
                 defaultLanguage="javascript" 
-                defaultValue="// some comment"
+                defaultValue={defaultValue}
                 onMount={(editor, monaco) => {            
                     monaco.editor.defineTheme('one-dark', oneDarkProTheme as any)
                     monaco.editor.setTheme('one-dark')
